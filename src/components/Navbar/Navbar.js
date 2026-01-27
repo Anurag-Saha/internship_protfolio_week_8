59b09d4323844ec31c55d561ed04f9a3cefa854a
Anurag Saha <anuragsaha9183@gmail.com>
2026-01-27 12:11:37 +0530

    homescreen and navbar updated

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useCart } from "../../contexts/CartContext";
 import { useAuth } from "../../contexts/AuthContext";
 import "./Navbar.css";
@@ -7,18 +7,41 @@ const Navbar = () => {
   const { cart } = useCart();
   const { user, login, logout } = useAuth();
 
+  const totalQty = cart.reduce((sum, item) => sum + item.qty, 0);
+
   return (
-    <nav className="nav">
-      <Link to="/" className="logo">Trendsy</Link>
+    <nav className="navbar">
+      <div className="nav-inner">
 
-      <div>
-        <Link to="/cart">Cart ({cart.length})</Link>
+        {/* LOGO */}
+        <NavLink to="/" className="logo">
+          Trendsy
+        </NavLink>
 
-        {user ? (
-          <button onClick={logout}>Logout</button>
-        ) : (
-          <button onClick={login}>Login</button>
-        )}
+        {/* LINKS */}
+        <div className="nav-links">
+          <NavLink to="/shop" className="nav-link">
+            Shop
+          </NavLink>
+
+          <NavLink to="/cart" className="nav-link cart-link">
+            Cart
+            {totalQty > 0 && (
+              <span className="cart-badge">{totalQty}</span>
+            )}
+          </NavLink>
+
+          {user ? (
+            <button className="nav-btn" onClick={logout}>
+              Logout
+            </button>
+          ) : (
+            <button className="nav-btn" onClick={login}>
+              Login
+            </button>
+          )}
+        </div>
+
       </div>
     </nav>
   );
